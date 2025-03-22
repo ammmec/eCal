a9b4bdadd2a714d359e612111b53b9db61afa635
eli <e.ventura.gabarro@student.tue.nl>
2025-03-22 17:19:40 +0100

    DS-22 All Classrooms Automated

--- schedule_formatter.py
+++ schedule_formatter.py
@@ -4,41 +4,42 @@ from datetime import datetime
 from datetime import timedelta
 
 CLIENT_ID = "t7SuHCjdalstLwG6ElnrJgn8URLbMYIwAOrkzcIa" # Public application of the Racó API
-FORMAT = "ics"  # ics is easier to treat
-
-# Classroom's schedule. To get all of them they should be added in a list and do an API call for each, or not add a filter for classroom 
-CLASS = "A6001"
+FORMAT = "ics"  # ics is easier to treat for calendars
 
 # Get today's schedule (start_date <= today < end_date)
-today = datetime.now() - timedelta(days=4)
+today = datetime.now() # - timedelta(days=4) # Testing in the weekend
 START = today.strftime("%Y-%m-%d")
 END = (today + timedelta(days=1)).strftime("%Y-%m-%d")
 
 # API URL with the necessary additions
-url = f"https://api.fib.upc.edu/v2/aules/{CLASS}/reserves/?format={FORMAT}&client_id={CLIENT_ID}&data_inici={START}&data_fi={END}"
+url = f"https://api.fib.upc.edu/v2/aules/?client_id={CLIENT_ID}&format=json"
 
-response = requests.get(url) # Obtain the result from API
+response = requests.get(url) # Obtain the classroom list from API
 
 if response.status_code == 200:
-    # Parse the ICS content
-    calendar = icalendar.Calendar.from_ical(response.text)
-    classes = []
+    classrooms = response.json().get("results")
+    for room in classrooms: # Parse clasrooms
+        print(room.get('id'))
 
-    # Extract classes
-    for event in calendar.walk("VEVENT"):
-        start_time = int(event.get("DTSTART").dt.strftime("%#H"))                   # Get start time
-        end_time = int((event.get("DTEND").dt + timedelta(minutes=1)).strftime("%#H"))   # Get end time (rounded to the hour)
-        name = str(event.get("DESCRIPTION")) if event.get("DESCRIPTION") != None else str(event.get("SUMMARY"))         # Class' name
-        #location = event.get("LOCATION")       # Get class location (already known)
+        # Specific classroom schedule
+        url = room.get('reserves').replace('json', FORMAT) + f"&client_id={CLIENT_ID}&data_inici={START}&data_fi={END}"
+        response = requests.get(url) # Obtain the schedule from API
 
-        #print(f"Class: {name}")
-        #print(f"Time: {start_time}-{end_time}")
-        # print(f"Location: {location}")
-        #print("-" * 40)
+        if response.status_code == 200:
+            calendar = icalendar.Calendar.from_ical(response.text)
+            classes = []
+            # Extract classes
+            for event in calendar.walk("VEVENT"):
+                start_time = int(event.get("DTSTART").dt.strftime("%#H"))                   # Get start time
+                end_time = int((event.get("DTEND").dt + timedelta(minutes=1)).strftime("%#H"))   # Get end time (rounded to the hour)
+                name = str(event.get("DESCRIPTION")) if event.get("DESCRIPTION") != None else str(event.get("SUMMARY"))         # Class' name
+                #location = event.get("LOCATION")       # Get class location (already known)
 
-        classes.append((name, start_time, end_time))
-    classes = sorted(classes, key=lambda x: x[1])
-    print(classes)
+                classes.append((name, start_time, end_time))
+            classes = sorted(classes, key=lambda x: x[1]) # Sort by start time of class
+            print(classes)
+        else:
+            print(f"Error {response.status_code}: {response.text}")
 
 else:
     print(f"Error {response.status_code}: {response.text}")
